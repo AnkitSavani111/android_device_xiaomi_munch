3a70763a0ab7601a70a179836520a5d4b94526cf
AnkitSavani111 <ankitmsavani12345@gmail.com>
2025-02-03 20:00:12 +0530

    munch: Adapt tree to ProjectPixelage
    Signed-off-by: AnkitSavani111 <ankitmsavani12345@gmail.com>

--- pixelage_munch.mk
+++ pixelage_munch.mk
@@ -11,19 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelage stuff.
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
 # Inherit extra stuff
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
-# Evolution-X Flags
-EVO_BUILD_TYPE := COMMUNITY
+# Pixelage Flags
+PIXELAGE_MAINTAINER := Ankit Savani
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_munch
+PRODUCT_NAME := pixelage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
