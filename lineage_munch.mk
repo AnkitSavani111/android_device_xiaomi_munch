rik-x777/device

munch: Adapt tree to Evolution-X

lineage_munch.mk
@@ -8,17 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Evolution-X Flags
+EVO_BUILD_TYPE := COMMUNITY
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := 22021211RC
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F4
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 

AnkitSavani111/android_device_xiaomi_munch

lineage_munch.mk
@@ -8,20 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
 # Inherit extra stuff
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Evolution-X Flags
+EVO_BUILD_TYPE := Unofficial
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := 22021211RC
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F4
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 